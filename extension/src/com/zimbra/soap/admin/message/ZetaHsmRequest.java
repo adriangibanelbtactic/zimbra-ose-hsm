36e7108f35f6e07b8b6957a2d16a183890f479da
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2023-10-10 17:34:43 +0200

    Admin Extension: Draft skeleton for ZetaHsmExtension.
    Based roughly on zmdedupe files:
    
    - BlobDeduperUtil.java : Cli
    - DedupeBlobsRequest.java : Request class
    - DedupeBlobsResponse.java : Response class
    - DedupeBlobs.java: Admin endpoint
    - BlobDeduper.java: Class that actually makes the work
    - AdminService.java: Endpoint is registered as an Admin Servlet
    
    Equivalent ZetaHsmExtension files:
    
    - ZetaHsmUtil.java : Cli
    - HsmRequest.java : Request class
    - HsmResponse.java : Response class
    - Hsm.java : Admin endpoint
    - ZetaHsm.java : Class that actually makes the work
    - ZetaHsmExtension.java : Endpoint is registered as an Admin Servlet (1)
    - ZetaHsmAdminService.java : Endpoint is registered as an Admin Servlet (2)

---
+++ extension/src/com/zimbra/soap/admin/message/ZetaHsmRequest.java
@@ -0,0 +1,95 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra OSE HSM Extension
+ * Copyright (C) 2023 BTACTIC, S.C.C.L.
+ *
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2012, 2013, 2014, 2016 Synacor, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+
+package com.zimbra.soap.admin.message;
+
+import java.util.Collections;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.google.common.base.MoreObjects;
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.soap.admin.message.AutoProvTaskControlRequest.Action;
+import com.zimbra.soap.admin.type.IntIdAttr;
+
+import com.btactic.hsm.soap.AdminConstants;
+
+/**
+ * @zm-api-command-auth-required true
+ * @zm-api-command-admin-auth-required true
+ * @zm-api-command-description Perform HSM.
+ */
+@XmlAccessorType(XmlAccessType.NONE)
+@XmlRootElement(name=AdminConstants.E_HSM_REQUEST)
+public class ZetaHsmRequest {
+
+    @XmlEnum
+    public static enum HsmAction {
+        start,
+        status,
+        stop,
+        reset;
+
+        public static Action fromString(String action) throws ServiceException {
+            try {
+                return Action.valueOf(action);
+            } catch (IllegalArgumentException e) {
+                throw ServiceException.INVALID_REQUEST("unknown action: " + action, e);
+            }
+        }
+    }
+
+    /**
+     * @zm-api-field-description Action to perform - one of <b>start|status|stop</b>
+     */
+    @XmlAttribute(name=com.zimbra.common.soap.AdminConstants.E_ACTION, required=true)
+    private final HsmAction action;
+
+    /**
+     * no-argument constructor wanted by JAXB
+     */
+    @SuppressWarnings("unused")
+    private ZetaHsmRequest() {
+        this((HsmAction)null);
+    }
+
+    public ZetaHsmRequest(HsmAction action) {
+        this.action = action;
+    }
+
+    public HsmAction getAction() {
+        return action;
+    }
+
+    public MoreObjects.ToStringHelper addToStringInfo(MoreObjects.ToStringHelper helper) {
+        return helper
+            .add("action", action);
+    }
+
+    @Override
+    public String toString() {
+        return addToStringInfo(MoreObjects.toStringHelper(this)).toString();
+    }
+}
